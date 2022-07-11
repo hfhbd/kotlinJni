0b43a5bd62bd46e6979a995ddebdd92583d7acab
hfhbd <hfhbd@users.noreply.github.com>
2022-07-11 17:10:24 +0200

    Fix getString

--- helloc/src/nativeMain/kotlin/sample/Hello.kt
+++ helloc/src/nativeMain/kotlin/sample/Hello.kt
@@ -4,12 +4,12 @@ import jni.*
 import kotlinx.cinterop.*
 
 @CName("Java_sample_Main_hello")
-fun hello(env: JNIEnv, obj: jobject, from: jstring) {
-    val getStringUTFChars = requireNotNull(env.pointed.GetStringUTFChars) {
+fun hello(env: CPointer<JNIEnvVar>, obj: jobject, from: jstring) {
+    val getStringUTFChars = requireNotNull(env.pointed.pointed!!.GetStringUTFChars) {
         "GET FAILED"
     }
     val cString = getStringUTFChars(env.reinterpret(), from, null)!!
     val kString = cString.toKString()
-    env.pointed.ReleaseStringUTFChars!!(env.reinterpret(), from, cString)
+    env.pointed.pointed!!.ReleaseStringUTFChars!!(env.reinterpret(), from, cString)
     println("HELLO FROM JNI: $kString")
 }
