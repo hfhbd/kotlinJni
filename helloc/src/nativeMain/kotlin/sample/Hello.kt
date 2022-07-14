81ea875c1a16c4e075dde19660fcc4b037a7a643
hfhbd <mybztg@icloud.com>
2022-07-14 09:03:28 +0200

    JNI Kotlin: extract getString

--- helloc/src/nativeMain/kotlin/sample/Hello.kt
+++ helloc/src/nativeMain/kotlin/sample/Hello.kt
@@ -5,9 +5,14 @@ import kotlinx.cinterop.*
 
 @CName("Java_sample_Main_hello")
 fun hello(env: CPointer<JNIEnvVar>, obj: jobject, from: jstring, repeat: jint): Int {
-    val getStringUTFChars = env.pointed.pointed!!.GetStringUTFChars!!
-    val cString = getStringUTFChars(env.reinterpret(), from, null)!!
+    val from = env.getString(from)
+    return hello("JNI: $from", repeat)
+}
+
+private fun CPointer<JNIEnvVar>.getString(string: jstring): String {
+    val getStringUTFChars = pointed.pointed!!.GetStringUTFChars!!
+    val cString = getStringUTFChars(reinterpret(), string, null)!!
     val kString = cString.toKString()
-    env.pointed.pointed!!.ReleaseStringUTFChars!!(env.reinterpret(), from, cString)
-    return hello("JNI: $kString", repeat)
+    pointed.pointed!!.ReleaseStringUTFChars!!(reinterpret(), string, cString)
+    return kString
 }
