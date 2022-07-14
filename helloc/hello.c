6511170413cc0d27dcb098ae264c2bc23e281d7f
Philip Wedemann <22521688+hfhbd@users.noreply.github.com>
2022-07-14 09:04:41 +0200

    C: Add missing java package

--- helloc/hello.c
+++ helloc/hello.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "NativeSample.h"
 
-JNIEXPORT int JNICALL Java_Main_hello(JNIEnv *env, jobject obj, jstring from, jint repeat) {
+JNIEXPORT int JNICALL Java_sample_Main_hello(JNIEnv *env, jobject obj, jstring from, jint repeat) {
   const char *f = (*env)->GetStringUTFChars(env, from, NULL);
     for(int x = 0; x < repeat; x++) {
       printf("Hello World from JNI: %s\n", f);
