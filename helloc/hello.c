fa14cf3727aea13cab2552a7226322e3e0ab4da2
hfhbd <hfhbd@users.noreply.github.com>
2022-07-23 21:32:17 +0200

    Add CI for manually C

--- helloc/hello.c
+++ helloc/hello.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
 #include "NativeSample.h"
 
-extern void HELLO(void);
-
 JNIEXPORT int JNICALL Java_sample_Main_hello(JNIEnv *env, jobject obj, jstring from, jint repeat) {
   const char *f = (*env)->GetStringUTFChars(env, from, NULL);
     for(int x = 0; x < repeat; x++) {
       printf("Hello World from JNI: %s\n", f);
     }
   (*env)->ReleaseStringUTFChars(env, from, f);
-  HELLO();
   return repeat;
 }
